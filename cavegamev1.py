b27213155709ffc95139d54133210e915fdec7d4
Graydon Armstrong <seeds77222@hotmail.com>
2013-05-22 20:35:25 -0400

    Created the intro message for the game

--- cavegamev1.py
+++ cavegamev1.py
@@ -8,6 +8,10 @@ def main():
     displayIntro()
     
 def displayIntro():
-    print("Hello World!")
+    print("You wake up to klaxons wailing on the bridge of your starship.")
+    print("You look around and see that the rest of your crew have not survived the impact.")
+    print("The ships computer announces that the boarding party from the unknown vessel is now onboard")
+    print("You will need to escape the ship the get to safety.")
+    print("Will you go left or right from the bridge? ")
     
 if __name__ == "__main__": main()
